f0c09403dddba917a3a7ee5a83fa42d41c0ea2b2
Yury Gargay <yury.gargay@gmail.com>
2017-11-07 21:50:12 +0100

    Normalize URL
    We only need `v` variable in query string, others should be skipped

--- popup.js
+++ popup.js
@@ -56,11 +56,24 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+function qs(query, variable) {
+    var vars = query.substring(1).split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     if (url.includes("youtube") && url.includes("watch")) {
       renderStatus('Performing Tube Streamer');
-      var streamUrl = 'http://tubestreamer.ru/stream/' + b32encode(url);
+      var p = document.createElement('a');
+      p.href = url;
+      normalized_url = p.protocol + "//" + p.host + p.pathname + "?" + "v=" + qs(p.search, "v");
+      var streamUrl = 'http://tubestreamer.ru/stream/' + b32encode(normalized_url);
       chrome.tabs.create({ url: streamUrl });
     } else{
       renderStatus('It is not YouTube.');
